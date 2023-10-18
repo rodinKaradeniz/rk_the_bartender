076c49bc923e86460c46547ea1ffe34caab6f2e0
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2023-10-18 13:49:22 -0400

    set weather api calls

--- weather.py
+++ weather.py
@@ -1,18 +1,38 @@
 import requests
 
-API_KEY = ""
+API_KEY = "abcdef"
 
-# get weather by U.S. zip code
-API_URL = ('http://api.openweathermap.org/data/2.5/weather?zip={},us&mode=json&units=imperial&appid={}')
+# get coordinates by location info - format: city, state code, country code, limit (optional), api key
+API_URL_GEO = "http://api.openweathermap.org/geo/1.0/direct?q={},{},{}&limit={}&appid={}"
 
-def weather_query_api(zip):
-    """submit the API query using variables for zip and API_KEY"""
+# get weather by coordinates - format: latitude, longitude, api key
+API_URL_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'
+
+def geocode_query_api(city='Toronto', state="ON", country="CA"):
+    """submit the API query using variables for the location info and API_KEY"""
     try:
         # print(API_URL.format(zip, API_KEY))
-        data = requests.get(API_URL.format(zip, API_KEY)).json()
+        data = requests.get(API_URL_GEO.format(city, state, country, "", API_KEY)).json()[0]
+        lat, lon = data['lat'], data['lon']
+        return lat, lon
 
     except Exception as exc:
         print(exc)
-        data = None
 
-    return data
+def weather_query_api(city='Toronto', state="ON", country="CA"):
+    """submit the API query using variables for zip and API_KEY"""
+    try:
+        # print(API_URL.format(zip, API_KEY))
+        lat, lon = geocode_query_api(city, state, country)
+        data = requests.get(API_URL_WEATHER.format(lat, lon, API_KEY)).json()
+        location = data['name']
+        weather = data['weather'][0]['description']
+        temperature = data['main']['temp']
+        return f"Location: {location}, Weather: {weather}, Temperature: {temperature} Fahrenheit"
+
+    except Exception as exc:
+        print(exc)
+
+if __name__ == '__main__':
+    print(geocode_query_api())
+    print(weather_query_api())
