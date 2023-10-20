af00892b4bbfbe686095c432841faedc37bab2c4
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2023-10-20 19:49:48 -0400

    db functions and data processing

---
+++ db/csv_utils.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+
+def update_alcoholic_col(a):
+    return a == "Alcoholic"
+
+def refactor_cocktail_df(df: pd.DataFrame) -> pd.DataFrame:
+    # Drop last row
+    df = df.drop(df.index[-1])
+
+    # Drop unused columns
+    df = df[['strDrink', 'strAlcoholic', 'strDrinkThumb', 'strGlass', 'strInstructions'] +
+            [f'strIngredient{i}' for i in range(1,16)] +
+            [f'strMeasure{i}' for i in range(1,16)]]
+
+    # Remove duplicates
+    df = df.drop_duplicates()
+
+    # Change "Alcoholic" col to True/False
+    df['strAlcoholic'] = df['strAlcoholic'].apply(update_alcoholic_col)
+
+    # Convert types of columns
+    df['strDrink'] = df['strDrink'].astype('string')
+    df['strAlcoholic'] = df['strAlcoholic'].astype('bool')
+    df['strDrinkThumb'] = df['strDrinkThumb'].astype('string')
+    df['strGlass'] = df['strGlass'].astype('string')
+    df['strInstructions'] = df['strInstructions'].astype('string')
+    for i in range(1, 16):
+        df[f"strIngredient{i}"] = df[f"strIngredient{i}"].astype('string')
+        df[f"strMeasure{i}"] = df[f"strMeasure{i}"].astype('string')
+
+    # Relabel columns except ingredients and measures
+    relabel = {
+        'strDrink': 'name',
+        'strAlcoholic': 'alcoholic',
+        'strDrinkThumb': 'image_url',
+        'strGlass': 'glass_type',
+        'strInstructions': 'recipe'
+        }
+    for old, new in relabel.items():
+        df = df.rename(columns = {old: new})
+
+    # Rename ingredient columns
+    for i in range(1, 16):
+        df = df.rename(columns = {f"strIngredient{i}": f"ingredient{i}"})
+
+    # Rename measure columns
+    for i in range(1, 16):
+        df = df.rename(columns = {f"strMeasure{i}": f"measure{i}"})
+
+    return df
+
+
+if __name__ == "__main__":
+    data = pd.read_csv('db/raw/all_drinks.csv', index_col=0)
+    new_data = refactor_cocktail_df(data)
+    new_data.to_csv('db/processed/cocktails.csv', index=True)
---
+++ db/sql_utils.py
@@ -0,0 +1,63 @@
+import os.path
+import sqlite3
+import pandas as pd
+
+def initialize_cocktails_db():
+    # TODO: test this
+
+    db_name = "db/sql/cocktails.db"
+
+    conn = sqlite3.connect(db_name) 
+    c = conn.cursor()
+
+    columns = [
+        "name TEXT",
+        "alcoholic TEXT",
+        "image_url TEXT",
+        "glass_type TEXT",
+        "recipe TEXT"
+    ] + [f"ingredient{i}" for i in range(1,16)] + [f"measure{i}" for i in range(1,16)]
+
+    create_table_cmd = f"CREATE TABLE IF NOT EXISTS cocktail ({','.join(columns)})"
+    c.execute(create_table_cmd)
+                        
+    conn.commit()
+
+
+def upload_cocktails_db(df):
+    # TODO: test this
+
+    # Check if database exists, then connect
+    db_name = "db/sql/cocktails.db"
+    if os.path.isfile(db_name):
+        conn = sqlite3.connect(db_name)
+    else:
+        raise Exception("Database does not exist")
+
+    # Upload dataframe to sql
+    df.to_sql(db_name, con=conn, schema='cocktail', index=True, if_exists='append')
+
+
+def load_cocktails_db(ticker: str, debug=False):
+    # TODO: Fix and finish this
+
+    db_name = "db/sql/cocktails.db"
+    if os.path.isfile(db_name):
+        conn = sqlite3.connect(db_name)
+
+        sql_query = pd.read_sql_query ('''
+                               SELECT
+                               *
+                               FROM products
+                               ''', conn)
+
+        df = pd.DataFrame(sql_query, columns = ['product_id', 'product_name', 'price'])
+        print(df)
+
+    else:
+        raise Exception("Database does not exist")
+    
+
+if __name__ == "__main__":
+    # TODO: Test functions, create DB, complete load_db function
+    print(1)
