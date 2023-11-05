f44093c4149a17a597cf5d18306b1e16277ed593
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2023-11-04 20:44:02 -0400

    implemented available_cocktails

--- make_lemonade.py
+++ make_lemonade.py
@@ -4,10 +4,33 @@
 # with the given ingredients.
 
 import pandas as pd
+from data_processing.csv_utils import load_ingredients_to_cocktails
+
 
 def available_cocktails(ingredients):
     # Load data
     cocktails = pd.read_csv('db/csv/processed/cocktails.csv')
 
-    # Iteratively lookup for the cocktails that contain the ingredients
+    # Get ingredients_to_cocktails
+    ingredients_to_cocktails = load_ingredients_to_cocktails(cocktails)
+
+    # Score cocktails based on how many of the given ingredients they have
+    cocktail_scores = {}
+    for ingredient in ingredients:
+        for cocktail in ingredients_to_cocktails[ingredient]:
+            if cocktail in cocktail_scores:
+                cocktail_scores[cocktail] += 1
+            else:
+                cocktail_scores[cocktail] = 1
+
+    # Sort found cocktails according to their scores
+    sorted_cocktail_scores = sorted(cocktail_scores.keys(),
+                                    key = lambda x: cocktail_scores[x],
+                                    reverse=True)
     
+    # Return top cocktails with their scores
+    return [(i, cocktail_scores[i]) for i in sorted_cocktail_scores]
+
+    
+if __name__ == "__main__":
+    pass
